67a690428fef60556ac3d0646c9643b09bc3fe20
dypark97 <61134303+dypark97@users.noreply.github.com>
2022-03-04 13:34:30 +0900

    3/4 -1 migration to stateful

--- lib/controllers/map_controller.dart
+++ lib/controllers/map_controller.dart
@@ -5,18 +5,14 @@ import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
-const MODE_ADD = 0xF1;
-const MODE_REMOVE = 0xF2;
-const MODE_NONE = 0xF3;
-
 class MapController extends GetxController {
   static MapController get to => Get.find();
-
   final Completer<NaverMapController> nMapController = Completer();
-  final RxList<Marker> markers = <Marker>[].obs;
-  final RxString _name = '한양대학교'.obs;
 
-  final RxInt currentMode = MODE_NONE.obs;
+  final RxList<Marker> markers = <Marker>[].obs;
+  // final RxString _name = '한양대학교'.obs;
+
+  // final RxInt currentMode = MODE_NONE.obs;
 
   // void onMapTap(LatLng latLng) {
   //   if (currentMode.value == MODE_ADD) {
@@ -29,37 +25,4 @@ class MapController extends GetxController {
   //   }
   // }
 
-  void onMarkerTap(Marker? marker, Map<String, int?>? iconSize) {
-    int pos = markers.indexWhere((m) => m.markerId == marker?.markerId);
-    markers[pos].captionText = '선택됨';
-    _name.value = '선택됨';
-    // Get.dialog(Container(
-    //   child: Text('as'),
-    // ));
-    if (currentMode.value == MODE_REMOVE) {
-      markers.removeWhere((m) => m.markerId == marker?.markerId);
-    }
-  }
-
-  @override
-  void onInit() {
-    super.onInit();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      OverlayImage.fromAssetImage(
-        assetName: 'assets/icons/marker.png',
-      ).then((image) {
-        markers.add(Marker(
-            markerId: 'id',
-            position: LatLng(37.563600, 126.962370),
-            captionText: _name.value,
-            captionColor: Colors.indigo,
-            captionTextSize: 14.0,
-            icon: image,
-            anchor: AnchorPoint(0.5, 1),
-            width: 45,
-            height: 45,
-            onMarkerTab: onMarkerTap));
-      });
-    });
-  }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,37 +3,77 @@ import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:universiting/controllers/map_controller.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   final MapController _mapController = Get.put(MapController());
 
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      OverlayImage.fromAssetImage(
+        assetName: 'assets/icons/marker.png',
+      ).then((image) {
+        _mapController.markers.add(Marker(
+            markerId: 'id',
+            position: LatLng(37.563600, 126.962370),
+            captionText: '한양대학교',
+            captionColor: Colors.indigo,
+            captionTextSize: 14.0,
+            icon: image,
+            anchor: AnchorPoint(0.5, 1),
+            width: 45,
+            height: 45,
+            onMarkerTab: onMarkerTap));
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  NaverMap(
-                    initialCameraPosition:
-                        CameraPosition(target: LatLng(37.563600, 126.962370)),
-                    onMapCreated: onMapCreated,
-                    // onMapTap: _onMapTap,
-                    markers: _mapController.markers,
-                    initLocationTrackingMode: LocationTrackingMode.Follow,
-                  ),
-                ],
-              ),
+    return Scaffold(
+      // appBar: AppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                NaverMap(
+                  initialCameraPosition:
+                      CameraPosition(target: LatLng(37.563600, 126.962370)),
+                  onMapCreated: onMapCreated,
+                  // onMapTap: _onMapTap,
+                  markers: _mapController.markers,
+                  initLocationTrackingMode: LocationTrackingMode.Follow,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
   void onMapCreated(NaverMapController controller) {
     _mapController.nMapController.complete(controller);
+    setState(() {});
+  }
+
+  void onMarkerTap(Marker? marker, Map<String, int?>? iconSize) {
+    int pos = _mapController.markers
+        .indexWhere((m) => m.markerId == marker?.markerId);
+    _mapController.markers[pos].captionText = '선택됨';
+    setState(() {});
+    // _name.value = '선택됨';
+    // Get.dialog(Container(
+    //   child: Text('as'),
+    // ));
+    // if (currentMode.value == MODE_REMOVE) {
+    //   markers.removeWhere((m) => m.markerId == marker?.markerId);
+    // }
   }
 }
